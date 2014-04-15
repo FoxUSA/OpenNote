453ce2d8cf58925d4f2d9174729ac8384308b476
FoxUSA <jake@jakelis.com>
2014-04-15 00:31:45 -0700

    test

--- OpenNote/openNote/factories/folderFactory.js
+++ OpenNote/openNote/factories/folderFactory.js
@@ -1,23 +1,23 @@
 /**
  *Folder factory 
  */
-openNote.factory("folderFactory", function($resource, apiToken) {
-    var folder = $resource(config.servicePath+"/:id", { }, {
+openNote.factory("folderFactory", function($resource, userService) {
+    var folder = $resource(config.servicePath+"folder/1/:id", { }, {
         get: {
             method: "GET",
-            headers: { "token": apiToken}
+            headers: { "token": userService.getApiToken()}
         },
         save: {
             method: "POST",
-            headers: { "token": apiToken}
+            headers: { "token": userService.getApiToken()}
         },
         update: {
             method: "PUT",
-            headers: { "token": apiToken}
+            headers: { "token": userService.getApiToken()}
         },
         remove: {
             method: "DELETE",
-            headers: { "token": apiToken}
+            headers: { "token": userService.getApiToken()}
         }
     });
     return folder;
