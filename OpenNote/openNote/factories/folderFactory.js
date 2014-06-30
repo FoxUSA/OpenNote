f0ddf0fb6dc423e0b540ba9fbf397b7e9fe4ffb9
FoxUSA <jake@jakelis.com>
2014-06-29 19:49:04 -0700

    Made get call on folders a little cleaner

--- OpenNote/openNote/factories/folderFactory.js
+++ OpenNote/openNote/factories/folderFactory.js
@@ -5,7 +5,7 @@ openNote.factory("folderFactory", function($resource, userService, config) {
 	return $resource(config.servicePath()+"/folder/:id", {}, {//{} default params
         get: {
             method: "GET",
-            params:{levels: 1, includeNotes: true},//override default params
+            params:{levels: 1, includeNotes: true, includeNotesHTML: false},//override default params
             url: config.servicePath()+"/folder/"//override
         },
         save: {
