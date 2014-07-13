9a6e9d568f4d232df54275eeb37e0f16afd8ba17
FoxUSA <jake@jakelis.com>
2014-07-12 20:23:54 -0700

    https://github.com/FoxUSA/OpenNote/issues/54
    Find working

--- OpenNote/openNote/controllers/searchController.js
+++ OpenNote/openNote/controllers/searchController.js
@@ -1,9 +1,60 @@
-
-openNote.controller("searchController", function($scope, $rootScope, searchService) {
+/**
+ * Search
+ */
+openNote.controller("searchController", function(	$scope, 
+													$rootScope, 
+													searchService,
+													config,
+													$location) {
+	/**
+	 * Default valie
+	 */
+	$scope.searchRequest={
+			type: "Both",
+			field: "Both",
+			search: ""
+	};
 	
+	/**
+	 * Send search request to backend
+	 */
 	$scope.search = function(){
 		searchService.search($scope.searchRequest).then(function(data){
-			console.log(data);//TODO
+			$scope.currentFolder = data;
 		});
 	}
+	
+	/**
+	 * Load a folder
+	 * @param folder- the folder to load
+	 */
+	$scope.loadFolder = function(folder){
+		$scope.fadeOutFolders(function(){
+			$location.url("/folder/"+folder.id);
+		});
+	};
+	
+	/**
+	 * Load a note
+	 * @param note - load a note
+	 */
+	$scope.loadNote = function(note){
+		$scope.fadeOutFolders(function(){
+			$location.url("/note/"+note.id);
+		});
+	};
+	
+	/**
+	 * fade out all folders
+	 */
+	$scope.fadeOutFolders = function(callback){
+		if($scope.currentFolder.foldersInside.length>0)
+			$(".folder").fadeOut(config.fadeSpeedShort(),function(){
+				$scope.$apply(function(){
+					callback();
+				});
+			});
+		else
+			callback();
+	};
 });
