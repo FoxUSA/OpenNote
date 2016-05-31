fd5e985db21926f0a81b64faba16ee2fdf02164b
FoxUSA <jake@jakelis.com>
2016-05-30 19:31:06 -0700

    #99 tags complete

---
+++ OpenNote/openNote/controllers/tagController.js
@@ -0,0 +1,38 @@
+
+openNote.controller("tagController", function(	$scope,
+												$rootScope,
+												$location,
+												$routeParams,
+												storageService,
+												config,
+												tagService) {
+		/**
+		 * Load a note
+		 * @param note - load a note
+		 */
+		$scope.loadNote = function(note){
+			$(".note").fadeTo(config.fadeSpeedShort(),0,function(){
+				$location.url("/note/"+note._id);
+				$scope.$apply();
+			});
+		};
+
+		/**
+		 * Load tags
+		 */
+		var loadTags = function(){
+			tagService.getMap().then(function(map){
+				var tags = map.tags[$scope.tag];
+				var db = storageService.database();
+				tags.forEach(function(tag){
+					db.get(tag).then(function(note){
+						$scope.notes.push(note);
+						$scope.$apply();
+					});
+				});				
+			});
+		};
+		$scope.notes=[];
+		$scope.tag = $routeParams.tag;
+		loadTags();
+});
